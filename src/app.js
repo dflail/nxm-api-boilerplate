a82a12151fb32761f35bbf659168d789b5a5d0ad
Dan Flail <dan.flail.dev@gmail.com>
2020-06-22 14:47:50 -0400

    Reverted OS Locale

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const consoleUtils = require('./utils/console-utils');
 const dotenv = require('dotenv');
 const express = require('express');
-const osLocale = require('os-locale');
 
 dotenv.config({ path: './app.env' });
 
 const app = express();
-const locale = osLocale.sync();
 
 app.get('/', (req, res) => {
   res.send(`${process.env.APP_NAME} v.${process.env.API_VERSION} READY`);
